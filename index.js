a061f00d70d96ba09214b48aabb0d9ce1ca5dfbb
lieby weintraub <liebahinda@gmail.com>
2019-09-04 15:46:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function writeCards(names, event) {
   const array= []
   for ( let i = 0; i < names.length; i++) {
-    array.push(' Thank you, ${names[i]} for your wonderful ')
+    array.push(' Thank you, ${names[i]} for your wonderful ${event} gift!')
   }
   
 };
