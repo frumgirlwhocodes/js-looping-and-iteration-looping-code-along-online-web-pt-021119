1aa8481675898a2ac0b3f8e2374333a2c49f4a9e
lieby weintraub <liebahinda@gmail.com>
2019-09-04 15:47:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solutions in this file
 function writeCards(names, event) {
-  const array= []
+  const array= [];
   for ( let i = 0; i < names.length; i++) {
     array.push(' Thank you, ${names[i]} for your wonderful ${event} gift!')
   }
