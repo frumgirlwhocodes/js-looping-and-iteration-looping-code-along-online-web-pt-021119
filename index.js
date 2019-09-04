8084416277935a9b95b758dbc8bdc59f5deb33e1
lieby weintraub <liebahinda@gmail.com>
2019-09-04 15:43:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // Code your solutions in this file
 function writeCards(names, event) {
-  for ( let i = 0; i < names.length; i++)
+  const array= []
+  for ( let i = 0; i < names.length; i++) {
+    array.push()
+  }
   
 };
